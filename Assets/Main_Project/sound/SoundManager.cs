ca8014f81623abb3cb8f162c4efd05d405e33c63
DESKTOP-GLLTLB2\kanta <kantapon3692@gmail.com>
2022-11-04 03:53:34 +0700

    sound

--- Assets/Main_Project/sound/SoundManager.cs
+++ Assets/Main_Project/sound/SoundManager.cs
@@ -8,10 +8,7 @@ namespace kantapon.GameDev3.Chapter11
         [SerializeField] protected SoundSettings m_SoundSettings;
         
         public Slider m_SliderMasterVolume;
-        public Slider m_SliderMusicVolume;
-        public Slider m_SliderMasterSFXVolume;
-        public Slider m_SliderSFXVolume;
-        public Slider m_SliderUIVolume;
+        
 
         void Start()
         {
@@ -21,10 +18,7 @@ namespace kantapon.GameDev3.Chapter11
         private void InitialiseVolumes()
         {
             SetMasterVolume(m_SoundSettings.MasterVolume);
-            SetMusicVolume(m_SoundSettings.MusicVolume);
-            SetMasterSFXVolume(m_SoundSettings.MasterSFXVolume);
-            SetSFXVolume(m_SoundSettings.SFXVolume);
-            SetUIVolume(m_SoundSettings.UIVolume);
+            
         }
 
         public void SetMasterVolume(float vol)
@@ -36,38 +30,7 @@ namespace kantapon.GameDev3.Chapter11
             m_SliderMasterVolume.value = m_SoundSettings.MasterVolume;
         }
 
-        public void SetMusicVolume(float vol)
-        {
-            m_SoundSettings.AudioMixer.SetFloat(m_SoundSettings.MusicVolumeName ,vol);
-            m_SoundSettings.MusicVolume = vol;
-            m_SliderMusicVolume.value = m_SoundSettings.MusicVolume;
-        }
-
-        public void SetMasterSFXVolume(float vol)
-        {
-            m_SoundSettings.AudioMixer.SetFloat(m_SoundSettings.MasterSFXVolumeName ,vol);
-
-            m_SoundSettings.MasterSFXVolume = vol;
-            
-            m_SliderMasterSFXVolume.value = m_SoundSettings.MasterSFXVolume;
-        }
-
-        public void SetSFXVolume(float vol)
-        {
-            m_SoundSettings.AudioMixer.SetFloat(m_SoundSettings.SFXVolumeName ,vol);
-
-            m_SoundSettings.SFXVolume = vol;
-            
-            m_SliderSFXVolume.value = m_SoundSettings.SFXVolume;
-        }
-
-        public void SetUIVolume(float vol)
-        {
-            m_SoundSettings.AudioMixer.SetFloat(m_SoundSettings.UIVolumeName ,vol);
-
-            m_SoundSettings.UIVolume = vol;
-
-            m_SliderUIVolume.value = m_SoundSettings.UIVolume;
-        }
+        
+        
     }
 }
--- Assets/Main_Project/sound/SoundSettings.cs
+++ Assets/Main_Project/sound/SoundSettings.cs
@@ -12,26 +12,6 @@ namespace kantapon.GameDev3.Chapter11
         public string MasterVolumeName = "MasterVolume";
         [Range(-80,20)]
         public float MasterVolume;
-        
-        [Header("MusicVolume")]
-        public string MusicVolumeName = "MusicVolume";
-        [Range(-80,20)]
-        public float MusicVolume;
-        
-        [Header("MasterSFXVolume")]
-        public string MasterSFXVolumeName = "MasterSFXVolume";
-        [Range(-80,20)]
-        public float MasterSFXVolume;
-
-        [Header("SFXVolume")]
-        public string SFXVolumeName = "SFXVolume";
-        [Range(-80,20)]
-        public float SFXVolume;
-        
-        [Header("UIVolume")]
-        public string UIVolumeName = "UIVolume";
-        [Range(-80,20)]
-        public float UIVolume;
     }
 
 }
